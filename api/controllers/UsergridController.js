d450f675362bc1989efc77e279291ee3f2ca3a6f
Nikhil John <nikhiljohn1010@gmail.com>
2015-06-06 13:39:54 +0530

    first commit

---
+++ api/controllers/UsergridController.js
@@ -0,0 +1,21 @@
+/**
+ * UsergridController
+ *
+ * @description :: Server-side logic for managing usergrids
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+
+
+  /**
+   * `UsergridController.index()`
+   */
+  index: function (req, res) {
+    return res.json({
+      todo: 'index() is not implemented yet!'
+    });
+  }
+};
+
